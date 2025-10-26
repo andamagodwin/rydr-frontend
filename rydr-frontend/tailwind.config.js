3d98a51e5e4d51c8f902df126dd4859edeb25a91
JMinallah <jovia.minallahmatata@students.mak.ac.ug>
2025-10-26 06:26:43 +0300

    Add color variables to CSS and Tailwind configuration

--- rydr-frontend/tailwind.config.js
+++ rydr-frontend/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E6007A',
+        secondary: {
+          white: '#FFFFFF',
+          black: '#000000',
+        },
+      },
+    },
   },
   plugins: [],
 }
