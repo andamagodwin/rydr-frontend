650419f1b674fd59914db6a0f22d526b10595fb1
JMinallah <jovia.minallahmatata@students.mak.ac.ug>
2025-11-11 13:49:51 +0300

    clean up vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isVercel = process.env.VERCEL === '1' || process.env.VERCEL === 'true' || !!process.env.VERCEL
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: isVercel ? '/' : '/rydr-frontend/',
+  base: '/',
   plugins: [react()],
 })
